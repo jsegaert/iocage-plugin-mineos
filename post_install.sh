31348f0de508846531f8a05c7c0c0ee1f947aa23
jsegaert <jsegaert@yahoo.com>
2018-08-22 22:27:51 -0700

    Update post-install text

--- post_install.sh
+++ post_install.sh
@@ -45,17 +45,12 @@ fi
 
 echo
 cat <<EOF
-888b     d888 d8b                    .d88888b.   .d8888b.
-8888b   d8888 Y8P                   d88P" "Y88b d88P  Y88b
-88888b.d88888                       888     888 Y88b.
-888Y88888P888 888 88888b.   .d88b.  888     888  "Y888b.
-888 Y888P 888 888 888 "88b d8P  Y8b 888     888     "Y88b.
-888  Y8P  888 888 888  888 88888888 888     888       "888
-888   "   888 888 888  888 Y8b.     Y88b. .d88P Y88b  d88P
-888       888 888 888  888  "Y8888   "Y88888P"   "Y8888P"
+#---------------------------------------------------------------------#
+# Getting started with the MineOs plugin
+#---------------------------------------------------------------------#
 
 MineOS is a server front-end to ease managing Minecraft administrative tasks.
-For more info, see https://github.com/hexparrot/mineos-node
+For more information, see https://github.com/hexparrot/mineos-node
 
 The default logon for the Admin Portal is: mcserver / mcserver
 EOF
